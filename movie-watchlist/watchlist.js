ccc043cd83ecde8f6bb44880ff6b211cc326a348
gabr1234iel <106829590+gabr1234iel@users.noreply.github.com>
2023-05-21 00:00:47 +0800

    ui fixes

--- movie-watchlist/watchlist.js
+++ movie-watchlist/watchlist.js
@@ -5,3 +5,11 @@ const moviesInWatchlist = JSON.parse(localStorage.getItem("moviesOnWatchlist"));
 console.log(moviesInWatchlist);
 
 populatePage(moviesInWatchlist,false);
+
+const clearWatchlistEl = document.getElementById("clear-watchlist");
+clearWatchlistEl.addEventListener('click' , () =>{
+    localStorage.removeItem("moviesOnWatchlist");
+    const moviesEl = document.getElementById("movies");
+    moviesEl.innerHTML = '';
+});
+
